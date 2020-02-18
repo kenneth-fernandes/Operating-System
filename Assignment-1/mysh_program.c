4ea0dcfe25fc1cee95e44de5b170b7905c7e1abd
kenneth-fernandes <kenferns9339@gmail.com>
2020-02-17 19:20:49 -0500

    Kenneth: Added comments.

--- Assignment-1/mysh_program.c
+++ Assignment-1/mysh_program.c
@@ -1,3 +1,6 @@
+/*
+* @author Kenneth Peter Fernandes
+*/
 #include "cmdLine.h"
 #include "stdio.h"
 #include "stdlib.h"
@@ -10,43 +13,54 @@ pid_t pid;
 int ret;
 int status;
 
-void processCmd(char **token)
+/*
+* Funtion to process the command after receiving the command in tokens
+* @oaram tokens - Contains the shell commmand split into separate tokens
+*/
+void processCmd(char **tokens)
 {
 
-    
     if ((pid = fork()) == 0)
-    {
-        //Child process
+    { //Child process block
         status = 0;
 
-        if (execvp(token[0], token) == -1)
+        if (execvp(tokens[0], tokens) == -1)
         {
             fprintf(stderr, "Failure while performing exec.\n");
             exit(EXIT_FAILURE);
         }
     }
     else
-    {
-        //Parent process
+    { //Parent process block
         waitpid(pid, &status, 0);
-        
     }
 }
 
+/*
+* The entry point of the program
+*/
 int main()
 {
     char **tokens;
-    
+
+    // Intializing the tokens array by allocating memory
     initCmdLine();
     while (1)
     {
         printf("mysh>");
+
+        // Retrieving the command in terms of separate tokens
         tokens = getCmdLine();
+
         if (strcmp(tokens[0], "exit") == 0)
             break;
+        // Invoking the function to process the shell command
         processCmd(tokens);
     }
-    free(tokens);
+
+    
+
+    // Invoking the function to free up memory
     freeCmdLine();
     return 0;
 }
