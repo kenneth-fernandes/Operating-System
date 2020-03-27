2a23ef7da968fa46d8e1ce91aabdad2da73e204e
kenneth-fernandes <kenferns9339@gmail.com>
2020-03-27 04:39:23 -0400

    Kenneth: Made changes to file.

--- Assignment-2/dev_ps_1.c
+++ Assignment-2/dev_ps_1.c
@@ -8,13 +8,21 @@
 #include <linux/init.h>
 #include <linux/sched.h>
 #include <linux/sched/signal.h>
+#include <linux/uaccess.h>
+#include <linux/miscdevice.h>
+#include <linux/slab.h>
 
 /* Definition of the structure task_struct FOR for processes/tasks in sched.h  */
 struct task_struct *process;
 
+static int majorNumber;
+static int chrBuffrSize = 256;
+static int index = 0;
+char **resultBufferArr;
+
 // The prototype functions for the character driver -- must come before the struct definition
-static int dev_open(struct inode *, struct file *);
-static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static int device_open(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
 
 static struct file_operations fops = {
     .owner = THIS_MODULE,
@@ -23,19 +31,44 @@ static struct file_operations fops = {
 
 static struct miscdevice process_lst_device = {
     .minor = MISC_DYNAMIC_MINOR,
-    .name = "Process List Device",
+    .name = "process_lst",
     .fops = &fops};
+
+static ssize_t device_read(struct file *file, char __user *out, size_t len, loff_t *offset)
+{
+    printk(KERN_INFO "%s", "\n Loading the Character Device for listing processes. \n");
+
+    return 0;
+}
+
+static int device_open(struct inode *inodep, struct file *filep)
+{
+    printk(KERN_INFO "Process_Lst_Device: Device has been opened");
+
+    resultBufferArr = kmalloc((sizeof(char *) * chrBuffrSize), GFP_KERNEL);
+
+    for_each_process(process)
+    {
+        sprintf(resultBufferArr[index], "\n PROCESS = %s | PID = %d | PPID = %d | CPU = %d | STATE = %ld ",
+                process->comm, process->pid, process->real_parent->pid, task_cpu(process), process->state);
+        index += 1;
+        /*printk(KERN_INFO "\n PROCESS = %s | PID = %d | PPID = %d | CPU = %d | STATE = %ld ",
+               process->comm, process->pid, process->real_parent->pid, task_cpu(process), process->state);*/
+    }
+    return 0;
+}
 
 /* Defining the function that is called when module is installed */
 int __init init_module()
 {
-    printk(KERN_INFO "%s", "\n Loading the Character Device for listing processes. \n");
+    majorNumber = misc_register(&process_lst_device);
 
-    for_each_process(process)
+    if (majorNumber < 0)
     {
-        printk(KERN_INFO "\n PROCESS = %s | PID = %d | PPID = %d | CPU = %d | STATE = %ld ",
-               process->comm, process->pid, process->real_parent->pid, task_cpu(process), process->state);
+        printk(KERN_ALERT "Process_Lst_Device: Failed to register a major number\n");
+        return majorNumber;
     }
+    printk(KERN_INFO "Process_Lst_Device: Registered correctly with major number %d\n", majorNumber);
 
     return 0;
 }
@@ -43,5 +76,6 @@ int __init init_module()
 /* Defining the function that is called when module is un-installed */
 void __exit cleanup_module()
 {
+    misc_deregister(&process_lst_device);
     printk(KERN_INFO "%s", "\n Un-installing the Character Device for listing processes. \n");
 }
